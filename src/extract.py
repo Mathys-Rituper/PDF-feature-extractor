c39a2cff49502ea27f3623f4896e754429364b59
Devserver <devserver@acabobgrenouille.fr>
2024-09-27 23:38:26 +0200

    Add features (still WIP)

---
+++ src/extract.py
@@ -0,0 +1,78 @@
+import hashlib
+import os
+import sys
+
+import pandas as pd
+import pdfid
+import pymupdf
+
+def hash_file_sha256(filename: str):
+    """ Returns the SHA256 hash of a file """
+    with open(filename,"rb") as f:
+        bytes = f.read()
+        return hashlib.sha256(bytes).hexdigest();
+
+async def extract_features_from_file(pdf_path : str, is_malicious : bool,
+                                     destination : pd.DataFrame):
+    try:
+        pymupdf_file = pymupdf.open(pdf_path)
+    except pymupdf.FileDataError:
+        print("opening: ",pdf_path)
+    else:
+        pdf_size = round(os.path.getsize(pdf_path) / 1024, 2)
+        hashed_file = hash_file_sha256(pdf_path)
+        title_len = len(os.path.basename(pdf_path))
+        try:
+            encryption = pymupdf_file.needs_pass or pymupdf_file.metadata["encryption"] is not None
+        except TypeError:
+            print("encryption: ",pdf_path)
+    
+        metadata_size = 0
+        pages = 0
+        header = 0
+        image_count = 0
+        text = 0
+        object_count = 0
+        font_count = 0
+        embedded_files_count = 0
+        embedded_files_average_size = 0
+        stream_keyword_count = 0
+        endstream_keyword_count = 0
+        stream_average_size = 0
+        xref_count = 0
+        obfuscation_count = 0
+        filter_count = 0
+        nestedfilter_object_count = 0
+        stream_object_count = 0
+        javascript_keyword_count = 0
+        js_keyword_count = 0
+        uri_keyword_count = 0
+        action_keyword_count = 0
+        aa_keyword_count = 0
+        openaction_keyword_count = 0
+        launch_keyword_count = 0
+        submitform_keyword_count = 0
+        acroform_keyword_count = 0
+        xfa_keyword_count = 0
+        jbig2decode_keyword_count = 0
+        richmedia_keyword_count = 0
+        trailer_keyword_count = 0
+        xref_keyword_count = 0
+        startxref_keyword_count = 0
+        children_count_average = 0
+        children_count_median = 0
+        children_count_variance = 0
+        leaves_count = 0
+        nodes_count = 0
+        degree = 0
+        degree_assortativity = 0
+        average_shortest_path = 0
+        average_clustering_coefficient = 0
+        density = 0
+
+        # print([hashed_file, pdf_size, title_len, encryption, metadata_size, pages, header, image_count, text, object_count, font_count, embedded_files_count, embedded_files_average_size, stream_keyword_count, endstream_keyword_count, stream_average_size, xref_count, obfuscation_count, filter_count, nestedfilter_object_count, stream_object_count, javascript_keyword_count, js_keyword_count, uri_keyword_count, action_keyword_count, aa_keyword_count, openaction_keyword_count, launch_keyword_count, submitform_keyword_count, acroform_keyword_count, xfa_keyword_count, jbig2decode_keyword_count, richmedia_keyword_count, trailer_keyword_count, xref_keyword_count, startxref_keyword_count, children_count_average, children_count_median, children_count_variance, leaves_count, nodes_count, degree, degree_assortativity, average_shortest_path, average_clustering_coefficient, density, is_malicious])
+        # add the extracted features to the DataFrame
+        destination.loc[len(destination)] = [hashed_file, pdf_size, title_len, encryption, metadata_size, pages, header, image_count, text, object_count, font_count, embedded_files_count, embedded_files_average_size, stream_keyword_count, endstream_keyword_count, stream_average_size, xref_count, obfuscation_count, filter_count, nestedfilter_object_count, stream_object_count, javascript_keyword_count, js_keyword_count, uri_keyword_count, action_keyword_count, aa_keyword_count, openaction_keyword_count, launch_keyword_count, submitform_keyword_count, acroform_keyword_count, xfa_keyword_count, jbig2decode_keyword_count, richmedia_keyword_count, trailer_keyword_count, xref_keyword_count, startxref_keyword_count, children_count_average, children_count_median, children_count_variance, leaves_count, nodes_count, degree, degree_assortativity, average_shortest_path, average_clustering_coefficient, density, is_malicious]
+
+
+
---
+++ src/main.py
@@ -0,0 +1,53 @@
+import asyncio
+from extract import extract_features_from_file
+import os
+import sys
+
+import pandas as pd
+
+async def main():
+    # Check if the user has properly provided the paths for both the benign and malicious PDF files
+    if len(sys.argv) != 3:
+        print("Usage: python3 extract.py <benign_pdf_path> <malicious_pdf_path>")
+        sys.exit(1)
+    # if one of the paths cannot be read or does not contain at least one PDF file, exit the program
+    if (not os.path.exists(sys.argv[1]) or not os.path.exists(sys.argv[2]) or
+       not os.path.isdir(sys.argv[1]) or not os.path.isdir(sys.argv[2])):
+        print("One of the paths does not exist.")
+        sys.exit(1)
+    # Get the list of PDF files in the benign and malicious directories
+    benign_files = [os.path.join(sys.argv[1], f) for f in os.listdir(sys.argv[1])
+                                                    if f.endswith('.pdf')]
+    malicious_files = [os.path.join(sys.argv[2], f) for f in os.listdir(sys.argv[2])
+                                                    if f.endswith('.pdf')]
+
+    # If there are no PDF files in the benign or malicious directories, exit the program
+    if len(benign_files) == 0 :
+        print("No PDF files in the benign directory.")
+        sys.exit(1)
+    if len(malicious_files) == 0:
+        print("No PDF files in the malicious directory.")
+        sys.exit(1)
+
+    features = ['hashed_file','pdf_size', 'title_len', 'encryption', 'metadata_size', 'pages', 'header', 'image_count', 'text', 'object_count', 'font_count', 'embedded_files_count', 'embedded_files_average_size', 'stream_keyword_count', 'endstream_keyword_count', 'stream_average_size', 'xref_count', 'obfuscation_count', 'filter_count', 'nestedfilter_object_count', 'stream_object_count', 'javascript_keyword_count', 'js_keyword_count', 'uri_keyword_count', 'action_keyword_count', 'aa_keyword_count', 'openaction_keyword_count', 'launch_keyword_count', 'submitform_keyword_count', 'acroform_keyword_count', 'xfa_keyword_count', 'jbig2decode_keyword_count', 'richmedia_keyword_count', 'trailer_keyword_count', 'xref_keyword_count', 'startxref_keyword_count', 'children_count_average', 'children_count_median', 'children_count_variance', 'leaves_count', 'nodes_count', 'degree_average', 'degree_assortativity', 'average_shortest_path', 'average_clustering_coefficient', 'density', "is_malicious"]
+    df = pd.DataFrame(columns=features)
+    
+    # Extract features from the benign PDF files
+    print(f"Extracting features from {len(benign_files)} benign PDF files...")
+    for benign_file in benign_files:
+        await extract_features_from_file(benign_file, False, df)
+
+    # Extract features from the malicious PDF files
+    print(f"Extracting features from {len(malicious_files)} malicious PDF files...")
+    for malicious_file in malicious_files:
+        await extract_features_from_file(malicious_file, True, df)
+
+    # Save the extracted features to a CSV file
+    print(f"Feature extraction completed, saving {len(df)} samples to features.csv")
+    df.to_csv('features.csv', index=False)
+
+    print("Feature extraction completed.")
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
