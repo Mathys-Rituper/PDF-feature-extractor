e247c394a0a72f360fba9b1a59c657f509da4192
Devserver <devserver@acabobgrenouille.fr>
2024-09-29 19:33:25 +0200

    Fixed exeption handling

--- src/extract.py
+++ src/extract.py
@@ -22,7 +22,7 @@ class Threaded_dataframe:
             try:
                 self.dataframe.loc[len(self.dataframe)] = entry
             except Exception as e:
-                logging.error(e)
+                logging.exception(f"Could not write entry to dataframe" )
 
     def __len__(self):
         return len(self.dataframe)
@@ -43,7 +43,7 @@ def extract_features_from_file(pdf_path : str, is_malicious : bool,
         pymupdf_file = pymupdf.open(pdf_path)
         logging.info(f"Processing {pdf_path}")
     except pymupdf.FileDataError:
-        logging.error("opening: ",pdf_path)
+        logging.exception(f"Exception while opening: {pdf_path}")
     else:
         features['pdf_size'] = round(os.path.getsize(pdf_path) / 1024, 2)
         features['hashed_file'] = hash_file_sha256(pdf_path)
@@ -51,7 +51,7 @@ def extract_features_from_file(pdf_path : str, is_malicious : bool,
         try:
             features['encryption'] = pymupdf_file.needs_pass or pymupdf_file.metadata["encryption"] is not None
         except TypeError:
-            logging.error("encryption: " + pdf_path)
+            logging.exception(f"encryption check error for {pdf_path}")
     
         features['metadata_size'] = 0
         if isinstance(pymupdf_file.metadata, dict):
@@ -73,7 +73,8 @@ def extract_features_from_file(pdf_path : str, is_malicious : bool,
 
                 features['object_count'] += len(page.get_xobjects())
             features['font_count'] = len(fonts)
-        except Exception:
+        except Exception as e:
+            logging.exception(f"Exception while processing {pdf_path}")
             features['image_count'] = -1
             features['text'] = -1
             features['object_count'] = -1
@@ -112,41 +113,40 @@ def extract_features_from_file(pdf_path : str, is_malicious : bool,
         features['startxref_keyword_count'] = 0
 
         # The nodal properties are extracted from code inspired by Ran Liu et Al.'s work for their research paper "Evaluating Representativeness in PDF Malware Datasets: A Comparative Study and a New Dataset". We thank them for making this code available.
-        # features['children_count_average'] = -1
-        # features['children_count_median'] = -1
-        # features['children_count_variance'] = -1
-        # features['leaves_count'] = -1
-        # features['nodes_count'] = -1
-        # features['degree'] = -1
-        # features['degree_assortativity'] = -1
-        # features['average_shortest_path'] = -1
-        # features['average_clustering_coefficient'] = -1
-        # features['density'] = -1
-        # logging.info("extracting genome for ", pdf_path)
-        # try:
-        #     genomeObj = PdfGenome.load_genome(pdf_path)
-        #     paths = PdfGenome.get_object_paths(genomeObj)
-        #     G = nx.DiGraph()
-        #     for path in paths:
-        #         for i in range(len(path)-1):
-        #             G.add_edge(path[i], path[i+1])
-        #     children_count = [degree for _, degree in G.out_degree()]
-        #     features['children_count_average'] = np.mean(children_count)
-        #     features['children_count_median'] = np.median(children_count)
-        #     features['children_count_variance'] = np.var(children_count)
-        #     features['leaves_count'] = sum(1 for node in G.nodes() if G.out_degree(node) == 0)
-        #     features['nodes_count'] = G.number_of_nodes()
-        #     features['degree'] = sum(dict(G.degree()).values()) / G.number_of_nodes()
-        #     features['degree_assortativity'] = nx.degree_assortativity_coefficient(G.to_undirected())
-        #     features['average_shortest_path'] = nx.average_shortest_path_length(G.to_undirected())
-        #     features['average_clustering_coefficient'] = nx.average_clustering(G.to_undirected())
-        #     features['density'] = nx.density(G)
-        # except Exception as e:
-        #     logging.info("genome error for: ",pdf_path, e)
-        #     #raise e
+        features['children_count_average'] = -1
+        features['children_count_median'] = -1
+        features['children_count_variance'] = -1
+        features['leaves_count'] = -1
+        features['nodes_count'] = -1
+        features['degree'] = -1
+        features['degree_assortativity'] = -1
+        features['average_shortest_path'] = -1
+        features['average_clustering_coefficient'] = -1
+        features['density'] = -1
+        try:
+            # TODO FIXME all of this is crap
+            genomeObj = PdfGenome.load_genome(pdf_path)
+            paths = PdfGenome.get_object_paths(genomeObj)
+            G = nx.DiGraph()
+            for path in paths:
+                for i in range(len(path)-1):
+                    G.add_edge(path[i], path[i+1])
+            children_count = [degree for _, degree in G.out_degree()]
+            features['children_count_average'] = np.mean(children_count)
+            features['children_count_median'] = np.median(children_count)
+            features['children_count_variance'] = np.var(children_count)
+            features['leaves_count'] = sum(1 for node in G.nodes() if G.out_degree(node) == 0)
+            features['nodes_count'] = G.number_of_nodes()
+            features['degree'] = sum(dict(G.degree()).values()) / G.number_of_nodes()
+            features['degree_assortativity'] = nx.degree_assortativity_coefficient(G.to_undirected())
+            features['average_shortest_path'] = nx.average_shortest_path_length(G.to_undirected())
+            features['average_clustering_coefficient'] = nx.average_clustering(G.to_undirected())
+            features['density'] = nx.density(G)
+        except Exception as e:
+            logging.exception(f"genome error for: {pdf_path}")
+     
 
         # logging.info([hashed_file, pdf_size, title_len, encryption, metadata_size, pages, header, image_count, text, object_count, font_count, embedded_files_count, embedded_files_average_size, stream_keyword_count, endstream_keyword_count, stream_average_size, xref_count, obfuscation_count, filter_count, nestedfilter_object_count, stream_object_count, javascript_keyword_count, js_keyword_count, uri_keyword_count, action_keyword_count, aa_keyword_count, openaction_keyword_count, launch_keyword_count, submitform_keyword_count, acroform_keyword_count, xfa_keyword_count, jbig2decode_keyword_count, richmedia_keyword_count, trailer_keyword_count, xref_keyword_count, startxref_keyword_count, children_count_average, children_count_median, children_count_variance, leaves_count, nodes_count, degree, degree_assortativity, average_shortest_path, average_clustering_coefficient, density, is_malicious])
         # add the extracted features to the DataFrame
-        #destination.loc[len(destination)] = [hashed_file, pdf_size, title_len, encryption, metadata_size, pages, header, image_count, text, object_count, font_count, embedded_files_count, embedded_files_average_size, stream_keyword_count, endstream_keyword_count, stream_average_size, xref_count, obfuscation_count, filter_count, nestedfilter_object_count, stream_object_count, javascript_keyword_count, js_keyword_count, uri_keyword_count, action_keyword_count, aa_keyword_count, openaction_keyword_count, launch_keyword_count, submitform_keyword_count, acroform_keyword_count, xfa_keyword_count, jbig2decode_keyword_count, richmedia_keyword_count, trailer_keyword_count, xref_keyword_count, startxref_keyword_count, children_count_average, children_count_median, children_count_variance, leaves_count, nodes_count, degree, degree_assortativity, average_shortest_path, average_clustering_coefficient, density, is_malicious]
         destination.add_entry(features)
         # logging.info(f"Finished processing: {pdf_path}")
